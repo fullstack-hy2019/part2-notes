ca3904dc6dd8fc49d0121b8901b8cdf134382891
Matti Luukkainen <mluukkai@iki.fi>
2019-01-09 09:12:26 +0200

    form added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react' 
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes) 
+  const [newNote, setNewNote] = useState('uusi muistiinpano...')
+
   const rows = () => notes.map(note =>
     <Note
       key={note.id}
@@ -9,12 +12,36 @@ const App = ({ notes }) => {
     />
   )
 
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Muistiinpanot</h1>
       <ul>
         {rows()}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">tallenna</button>
+      </form>      
     </div>
   )
 }
