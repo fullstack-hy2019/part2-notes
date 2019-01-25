fcd24c73ac91c16130438119f77a5b4cfed8395c
Matti Luukkainen <mluukkai@iki.fi>
2019-01-25 18:09:46 +0200

    part5 start

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science 2019</em>
+    </div> 
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     noteService
@@ -56,6 +73,19 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const login = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
   const rows = () => notesToShow.map(note =>
     <Note
       key={note.id}
@@ -70,6 +100,36 @@ const App = () => {
 
       <Notification message={errorMessage} />
 
+      <h2>Kirjaudu</h2>
+
+      <form onSubmit={login}>
+        <div>
+          käyttäjätunnus
+            <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          salasana
+            <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">tallenna</button>
+      </form>  
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           näytä {showAll ? 'vain tärkeät' : 'kaikki'}
@@ -79,13 +139,7 @@ const App = () => {
         {rows()}
       </ul>
 
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">tallenna</button>
-      </form>      
+      <Footer />
     </div>
   )
 }
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,16 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'Tätä muistiinpanoa ei ole palvelimelta',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting))
-  //return request.then(response => response.data)
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
@@ -18,8 +11,8 @@ const create = newObject => {
   return request.then(response => response.data)
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+const update = (newObject) => {
+  const request = axios.put(`${baseUrl}/${newObject.id}`, newObject)
   return request.then(response => response.data)
 }
 
