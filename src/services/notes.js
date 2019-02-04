520b2854976b5fbce1a52da9de57fd788865c103
Matti Luukkainen <mluukkai@iki.fi>
2019-02-04 11:43:45 +0200

    fix update route

--- src/services/notes.js
+++ src/services/notes.js
@@ -18,8 +18,8 @@ const create = newObject => {
   return request.then(response => response.data)
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+const update = (newObject) => {
+  const request = axios.put(`${baseUrl}/${newObject.id}`, newObject)
   return request.then(response => response.data)
 }
 
